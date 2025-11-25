621bcfec51648566ee206d4d6cd006d5be3cc21a
Anthony Catan-Cavery <a.catancavery@gmail.com>
2025-11-25 18:32:42 +0100

    Tp1 Agenda terminé - Routes, templates Twig et Bootstrap

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    // #[Route('/home', name: 'app_home')]
+    // public function index(): Response
+    // {
+    //     return $this->render('base.html.twig', [
+    //         'controller_name' => 'HomeController',
+    //     ]);
+    // }
+
+
+
+    #[Route('/home', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('home/home.html.twig', [
+            'controller_name' => 'HomeController'
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig', [
+            'controller_name' => 'HomeController'
+        ]);
+    }
+    
+}
