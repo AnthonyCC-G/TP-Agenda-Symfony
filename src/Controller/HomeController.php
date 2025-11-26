088abef26bb35fc5f12fca328b5fc49962f75602
Anthony Catan-Cavery <a.catancavery@gmail.com>
2025-11-26 16:21:15 +0100

    Ajout de Doctrine et connexion avec database et table contact

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Contact;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ContactRepository;
 
 final class HomeController extends AbstractController
 {
@@ -18,20 +21,56 @@ final class HomeController extends AbstractController
 
 
 
+    // #[Route('/home', name: 'app_home')]
+    // public function home(): Response
+    // {
+    //     return $this->render('home/home.html.twig', [
+    //         'controller_name' => 'HomeController'
+    //     ]);
+    // }
+
+    // #[Route('/contact', name: 'app_contact')]
+    // public function contact(): Response
+    // {
+    //     return $this->render('contact.html.twig', [
+    //         'controller_name' => 'HomeController'
+    //     ]);
+    // }
+
+    // #[Route('/test-insert', name: 'test_insert')]
+    // public function testInsert(EntityManagerInterface $entityManager): Response
+    // {
+    //     $contact = new Contact();
+    //     $contact->setNom('Le Barbare');
+    //     $contact->setPrenom('Connan');
+    //     $contact->setTelephone('0114776688');
+    //     $contact->setAdresse('avenue des vilains musclés');
+    //     $contact->setVille('GoMuscu');
+    //     $contact->setAge(25);
+
+    //     $entityManager->persist($contact);
+
+    //     $entityManager->flush();
+
+    //     return new Response('Contact enregistré !');
+    // }
+    
     #[Route('/home', name: 'app_home')]
-    public function home(): Response
+    public function home(ContactRepository $contactRepository): Response
     {
+        $contacts = $contactRepository->findAll();
+
         return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController'
+            'contacts' => $contacts
         ]);
     }
 
-    #[Route('/contact', name: 'app_contact')]
-    public function contact(): Response
-    {
-        return $this->render('contact.html.twig', [
-            'controller_name' => 'HomeController'
-        ]);
-    }
-    
+    #[Route('/contact/{id}', name: 'app_contact')]
+public function contact(Contact $contact): Response
+{
+    return $this->render('contact.html.twig', [
+        'contact' => $contact
+    ]);
+}
+
 }
